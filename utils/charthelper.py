662784cfbf96d2fd75efb2b2cd66c835a82a2866
sui84 <sui84@126.com>
2017-11-20 14:04:34 +0800

    Create charthelper.py

---
+++ utils/charthelper.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+import matplotlib.pyplot as plt  
+
+class ChartHelper(object):
+    def __init__(self):
+        bypass
+    
+if __name__ == '__main__':
+    plt.bar(left = (0,1),height = (1,0.5),width = 0.35)
+    plt.show()
